ebf1aecdd92f360b8d09285f9446ffd30857272e
田中健 <takeru.tt5672@gmail.com>
2019-08-25 19:27:19 +0900

    corresponds to more tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -14,6 +14,17 @@ x + y;
 };
 
 let result = add(five, ten);
+!-/*5;
+9 < 10 > 4;
+
+if (2 < 10) {
+	return true;
+} else {
+	return false;
+}
+
+12 == 12
+10 != 33
 `
 
 	tests := []struct {
@@ -56,6 +67,41 @@ let result = add(five, ten);
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "9"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "4"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "2"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.INT, "12"},
+		{token.EQ, "=="},
+		{token.INT, "12"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "33"},
 		{token.EOF, ""},
 	}
 
